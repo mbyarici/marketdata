21a96c8de93261dd21f4924e4adec6de32bdf543
Mustafa Yarıcı <mustafayarici@embaenergy.local>
2023-07-31 16:20:21 +0300

    excel

--- KGUP.py
+++ KGUP.py
@@ -14,16 +14,16 @@ import openpyxl
 import numpy as np
 
 
-#%%veri oku
+#%%data
 veri = pd.read_excel('marketveri.xlsx', sheet_name='secim')
 veri['date']=pd.to_datetime(veri['date'])
 
-#%% Sayfa 1 özet dataframe
+#%% data
 
 data=veri[["date","organizationShortName","toplam","dogalgaz","ruzgar","linyit","ithalKomur","barajli","mcp"]]
 data.columns=["Tarih","Katılımcı","Toplam KGÜP","Doğalgaz","Rüzgar","Linyit","İthal Kömür","Barajlı","PTF"]
 
-#%%org multi filtre
+#%%filter
 
 selected_organizations = st.multiselect('Organizasyon Seçiniz', data['Katılımcı'].unique())
 
@@ -37,45 +37,47 @@ selected_days = st.slider('Tarih Seçiniz', min_value=min(data['Tarih']).date(),
 
 selected_hours = st.slider('Saat Seçiniz', min_value=0, max_value=23, value=(0, 23))
 
-#%% tüm filtreler
+#%% filter
 
 filtered_data = data[(data['Katılımcı'].isin(selected_organizations)) &
                      (data['Tarih'].dt.date >= selected_days[0]) &
                      (data['Tarih'].dt.date <= selected_days[1]) &
                      (data['Tarih'].dt.hour >= selected_hours[0]) &
                      (data['Tarih'].dt.hour <= selected_hours[1])]
+
 tabledata=filtered_data.copy()
 filtered_data=filtered_data.loc[:, (filtered_data != 0).any(axis=0)]
+
 #%% charts
 
 if not filtered_data.empty:
     for fuel_type in filtered_data.columns[3:-1]:
         fig, ax = plt.subplots()
         
-        # Filter data for the current fuel type
+        # Filter
         fuel_data = filtered_data[['Tarih', 'Katılımcı', fuel_type]]
         
-        # Plot the data for each organization
+        # Plot
         for org in selected_organizations:
             org_data = fuel_data[fuel_data['Katılımcı'] == org]
             ax.plot(org_data['Tarih'], org_data[fuel_type], linewidth=2, label=org)
         
-        # Rotate x-axis tick labels for better readability
+        # better readability
         plt.xticks(rotation=45, ha='right')
         
-        # Set plot labels and title
+        # plot labels
         ax.set_xlabel('Tarih')
         ax.set_ylabel('KGÜP')
         ax.set_title(f'KGÜP {fuel_type}')
         
-        # Add separate legends for each organization
+        # legends
         ax.legend(loc='upper left')
         
-        # Format x-axis as readable date values
+        # Format
         ax.xaxis.set_major_locator(mdates.AutoDateLocator())
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
         
-        # Display the chart
+        # Display
         st.pyplot(fig)
 
 
