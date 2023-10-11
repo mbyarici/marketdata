96630c43a81301e30cf771e41652ad366fab99c3
Mustafa Yarıcı <mustafayarici@embaenergy.local>
2023-10-11 14:40:30 +0300

    update

--- KGUP.py
+++ KGUP.py
@@ -29,14 +29,14 @@ hide_st_style = """
             header {visibility: hidden;}
             </style>
             """
-st.set_page_config(page_title="EMBA", page_icon=":chart_with_upwards_trend:", layout="centered")
+st.set_page_config(page_title="EMBA", page_icon=":chart_with_upwards_trend:", layout="wide")
 
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
 
 #%%veri
-kgupsum=pd.read_csv('genel.csv',encoding='utf-8-sig',sep=";", decimal=",",index_col=False)
+kgupsum=pd.read_csv('C:/marketdata/genel.csv',encoding='utf-8-sig',sep=";", decimal=",",index_col=False)#C:/marketdata/
 kgupsum['tarih']=pd.to_datetime(kgupsum['tarih'])
 
 #%% data
@@ -65,33 +65,20 @@ filtered_data = data[(data['Tarih'].dt.date >= selected_days[0]) &
                      (data['Tarih'].dt.hour <= selected_hours[1])]
 tabledata=filtered_data.copy()
 filtered_data=filtered_data.loc[:, (filtered_data != 0).any(axis=0)]
-#%% charts
+
+
 
 for fuel_type in filtered_data.columns[1:]:
-    fig, ax = plt.subplots(figsize=(10, 4))
-    
-    # Filter data
+    fig = go.Figure()
     fuel_data = filtered_data[['Tarih',  fuel_type]]
-    ax.plot(fuel_data['Tarih'], fuel_data[fuel_type], linewidth=2, label=fuel_type)
+    if not fuel_data.empty:
 
-    
-    # better readability
-    plt.xticks(rotation=45, ha='right')
-    
-    # Set plot labels and title
-    ax.set_xlabel('Tarih')
-    ax.set_ylabel('KGÜP')
-    ax.set_title(f'{fuel_type} KGÜP')
-    
-    # separate legends
-    ax.legend(loc='upper left')
-    
-    # Format
-    ax.xaxis.set_major_locator(mdates.AutoDateLocator())
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
-    
-    # Display
-    st.pyplot(fig)
+        fig.add_trace(go.Scatter(x=fuel_data['Tarih'], y=fuel_data[fuel_type], name=fuel_type,mode='lines'))
+
+        fig.update_layout(title=fuel_type, height=500, yaxis=dict(title=dict(text="MWh"),side="left") )
+        
+        fig.update_xaxes(tickformat='%Y-%m-%d %H')
+        st.plotly_chart(fig,use_container_width=True)
 
 daily = tabledata.groupby(filtered_data['Tarih'].dt.date).agg({"Toplam":"sum","Doğalgaz":"sum","Rüzgar":"sum","Linyit":"sum","İthal Kömür":"sum","Barajlı":"sum","Akarsu":"sum","Diğer":"sum"})
 daily[0:] = daily[0:].astype(int)
